6b68b46c8bf40834ee3994022171a3df4b658fd8
bluefox182 <fergaldoss@gmail.com>
2021-05-13 14:09:03 -0500

    hour function set up

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,11 @@ function setDate() {
   const minsDegrees = (mins / 60) * 360 + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-  const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const hour = now.getSeconds();
+  const hourDegrees = (seconds / 60) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  setInterval(setDate, 1000); // equal to 1s
+
+  setDate();
 }
